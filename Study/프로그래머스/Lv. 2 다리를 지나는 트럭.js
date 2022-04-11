3ba0ffa192d6493b2d6717b19972f83b3548dafa
Kim Jeong In <>
2022-04-11 18:48:41 +0900

    프로그래머스 문제 풀이

---
+++ Study/프로그래머스/Lv. 2 다리를 지나는 트럭.js
@@ -0,0 +1,38 @@
+function solution(bridge_length, weight, truck_weights) {
+    var answer = 0;
+    let bridge = Array.from({ length: bridge_length }, (dr) => 0);
+    let curW = 0;
+    let curL = 0;
+
+    while (truck_weights.length != 0) {
+        answer++;
+
+        let shifts = bridge.shift();
+        if (shifts > 0) {
+            curW -= shifts;
+            curL -= 1;
+        }
+
+        if (curL < bridge_length && curW + truck_weights[0] <= weight) {
+            let input = truck_weights.shift();
+            curW += input;
+            curL += 1;
+            bridge.push(input);
+        } else {
+            bridge.push(0);
+        }
+    }
+
+    answer += bridge_length;
+
+    return answer;
+}
+
+let bridge_length = 2;
+let weight = 10;
+let truck_weights = [7, 4, 5, 6];
+console.log(solution(bridge_length, weight, truck_weights));
+
+// 다리에 올라갈 수 있는 트럭 수 bridge_length
+// 다리가 견딜 수 있는 무게 weight
+// 트럭 별 무게 truck_weights
--- Study/프로그래머스/해시_ 위장.js
+++ Study/프로그래머스/해시_ 위장.js
@@ -1,16 +1,21 @@
 function solution(clothes) {
     var answer = 1;
 
-    let clothObj = new Object();
+    let clothObj = new Map();
+
     clothes.forEach((dr) => {
-        clothObj[`${dr[1]}`] = clothObj[`${dr[1]}`] ? clothObj[`${dr[1]}`].concat([`${dr[0]}`]) : [`${dr[0]}`];
-    })
+        clothObj.set(`${dr[1]}`, clothObj.get(`${dr[1]}`) ? [...clothObj.get(`${dr[1]}`), ...[`${dr[0]}`]] : [`${dr[0]}`])
+    });
+    // 의상의 종류를 키로 의상의 이름을 배열 값으로 해시테이블을 만든다. 
 
-    for(var key in clothObj){
-        answer *= (clothObj[key].length+1);
+    for (let entry of clothObj) {
+        answer *= (entry[1].length + 1);
     }
+    answer-=1;
+    // 해시테이블의 Entry(각 데이터)를 돌며 배열 값의 길이 + 1을 곱해준다. 
+    // 1을 더해서 곱하는이유는 자기자신도 하나만 입는 경우의 수도 존재하기 때문 결과값에 1을 빼줘야함(아무것도 선택안한경우는 제외)
 
-    return answer-1;
+    return answer
 }
 
 let clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]];
