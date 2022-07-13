0edef5a6c3d289dd39c0866f4bcda668bca48576
Kim Jeong In <gsdgf1324@naver.com>
2022-07-13 14:10:38 +0900

    test

--- Study/백준/알고리즘 기초 1/쇠막대기 - 다시풀기.js
+++ Study/백준/알고리즘 기초 1/쇠막대기 - 다시풀기.js
@@ -2,6 +2,7 @@ function solution(input) {
     console.log(input);
 }
 
+
 // '/dev/stdin'
 const input = require('fs').readFileSync('stdin').toString().trim().split('\n')[0];
 console.log(solution(input));
